9c8580bb19e00b0341a6c40fa49e9fbc1e5726b7
tibor.kovacs <tibor.kovacs@strivacity.com>
2025-01-25 10:15:36 +0100

    test(shopping-service): add unit tests

---
+++ src/app/mocks/shopping-list.mock.ts
@@ -0,0 +1,11 @@
+import { CartItem, Product } from '../interfaces/product.interface';
+
+export const mockProducts: Array<Product> = [
+  { id: '1', name: 'Apple', price: 1000, availableAmount: 10, minOrderAmount: 1, img: 'apple.jpg' },
+  { id: '2', name: 'Windows', price: 800, availableAmount: 20, minOrderAmount: 2, img: 'windows.jpg' },
+];
+
+export const mockCartItems: Array<CartItem> = [
+  { id: '1', name: 'Apple', price: 1000, amount: 8 },
+  { id: '2', name: 'Windows', price: 800, amount: 2 },
+];
--- src/app/services/shopping.service.spec.ts
+++ src/app/services/shopping.service.spec.ts
@@ -1,18 +1,43 @@
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { beforeEach, describe, expect, it } from 'vitest';
 
+import { mockCartItems, mockProducts } from '../mocks/shopping-list.mock';
 import { ShoppingService } from './shopping.service';
 
-describe('ShoppingServiceService', () => {
+describe('ShoppingService', () => {
   let service: ShoppingService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ShoppingService, provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ShoppingService);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    const req = httpMock.expectOne(service['apiUrl']);
+    req.flush(mockProducts);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); // Ensure no outstanding requests
+  });
+
+  it('should create shopping service', () => {
+    expect(service).toBeInstanceOf(ShoppingService);
+  });
+
+  it('should product to be fetched from constructor', () => {
+    expect(service.productsLoading()).toBe(false);
+    expect(service.products()).toEqual(mockProducts);
+  });
+
+  it('should get the cart items from the service', () => {
+    expect(service.getCartItems()).toEqual([]);
+    service.cart.set(mockCartItems);
+    expect(service.getCartItems()).toEqual(mockCartItems);
   });
 });
